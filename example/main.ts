6d572a4c032845f8cb07be85c00c5fe8cfc89708
Tmier <itmier@126.com>
2024-03-25 22:31:13 +0800

    feat: init

---
+++ example/main.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: 王云飞
+ * @Date: 2023-02-23 16:31:41
+ * @LastEditTime: 2024-03-25 21:17:30
+ * @LastEditors: Tmier
+ * @Description:
+ *
+ */
+import * as BetterJs from '@/main'
+export default BetterJs
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: 王云飞
+ * @Date: 2023-02-13 14:50:43
+ * @LastEditTime: 2024-03-25 22:16:51
+ * @LastEditors: Tmier
+ * @Description:
+ *
+ */
+export const a = 999
+console.log(a)
